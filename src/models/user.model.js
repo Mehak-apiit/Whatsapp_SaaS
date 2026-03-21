86f0851fdf1cd270f864fe53ac2b733f0f8ea8ff
Mehak <mehak.apiit@gmail.com>
2026-03-21 14:34:06 +0530

    feat: implement register user API

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,4 +2,16 @@ import mongoose from "mongoose";
 const userSchmea = new mongoose.Schema({
     email:{
         type: String,
-        required: t
+        required: true,
+        unique: true
+
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+const User = mongoose.model("User",userSchmea);
+export default User;
---
+++ src/services/auth.service.js
@@ -0,0 +1,4 @@
+import UserRepository from "../repositories/user.repository.js";
+const userRepository = new UserRepository();
+class AuthService{}
+export default AuthService;
